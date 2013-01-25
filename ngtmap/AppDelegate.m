e2b726d06594712310d27eb0220a8233ab82b403
ReDetection <redetection@gmail.com>
2013-01-26 00:27:07 +0700

    пункт 4 TODO: вернуть пользователя туда, где он был до того, как нажал home

--- ngtmap/AppDelegate.m
+++ ngtmap/AppDelegate.m
@@ -116,8 +116,6 @@
      Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
      */
     [self.searchViewController.transportDataSource loadPeriodicalData];
-    [self.searchViewController.navigationController popToRootViewControllerAnimated:YES];
-    [self.favoritesViewController.navigationController popToRootViewControllerAnimated:YES];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
